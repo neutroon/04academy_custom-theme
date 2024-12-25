f7e9074b01a21dbc46b9d2c5e1d7d5be32ab7c1d
neutroon <191900312@ecu.edu.eg>
2024-12-25 17:57:23 +0200

    #

--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
 <?php
+
+
 function enqueue_custom_theme_assets() {
     // Enqueue the main stylesheet
     wp_enqueue_style('custom-theme-style', get_stylesheet_uri());
+    
+    // Enqueue SweetAlert2 JS
+    wp_enqueue_script('sweetalert2-script', 'https://cdn.jsdelivr.net/npm/sweetalert2@11', array('jquery'), null, true);
 
     // Enqueue the main JavaScript file
     wp_enqueue_script('custom-theme-script', get_template_directory_uri() . '/main.js', array(), '1.0', true);
